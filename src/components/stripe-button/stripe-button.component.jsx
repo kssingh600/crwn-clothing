46af279d1b4201bd0e72943d0f452d2ca85be518
Kuldeep Singh <kssingh600@gmail.com>
2021-05-26 14:03:48 +0530

    removing redux-logger from production build

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,29 @@
-import React from 'react' ;
-import StripeCheckout from 'react-stripe-checkout' ;
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100 ;
-    const publishableKey = 'pk_test_51Iv15jSHFeAFdPeoGUbIngEWaykTTtfrnWywR7ccJGkyvJ6NC5s6EYqc9fKJcuYD3PVF3vt9wrj9FZh30rGMLayJ00YHUR9r9u' ;
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
-    const onToken = token => {
-        console.log(token) ;
-        alert('Payment Successful') ;
-    }
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
 
-    return(
-        <StripeCheckout
-            label='Pay Now'
-            name='CRWN Clothing Ltd.'
-            billingAddress
-            shippingAddress
-            img='https://sendeyo.com/up/d/f3eb2117da'
-            description={`Your total is $${price} `}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publishableKey}
-        />
-    );
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
-export default StripeCheckoutButton ;
+export default StripeCheckoutButton;
